909b153fdebf868a536bceda582dca2937901da8
Amin Siddiqui <staticVoidMan@users.noreply.github.com>
2023-05-21 18:19:51 +0530

    #02 - using `re` module compile

--- challenge/02/html2markdown.py
+++ challenge/02/html2markdown.py
@@ -1,10 +1,14 @@
 import re
 
+ITALICS = re.compile(r'<em>(.+?)</em>')
+SPACES = re.compile(r'\s+')
+PARAGRAPHS = re.compile(r'<p>(.+?)</p>')
+LINKS = re.compile(r'<a href="(.+?)">(.+?)</a>')
+
 def html2markdown(html):
     '''Take in html text as input and return markdown'''
-    html = re.sub(r"</?em>", "*", html)
-    html = re.sub(r"\s+", " ", html)
-    html = re.sub(r"<p>", "\n\n", html).strip()
-    html = re.sub(r"</p>", "", html)
-    html = re.sub(r'<a href="(.+?)">(.+?)</a>', r"[\2](\1)", html)
-    return html
+    markdown = ITALICS.sub(r'*\1*', html)
+    markdown = SPACES.sub(r' ', markdown)
+    markdown = PARAGRAPHS.sub(r'\n\n\1', markdown)
+    markdown = LINKS.sub(r'[\2](\1)', markdown)
+    return markdown.strip()
