75a3a21fe1d93c60001033d4fd71ca0afdd0fbc7
Amin Siddiqui <staticVoidMan@users.noreply.github.com>
2023-05-21 19:18:40 +0530

    #03

--- challenge/03/pairwise_offset.py
+++ challenge/03/pairwise_offset.py
@@ -1,2 +1,11 @@
-def pairwise_offset(sequence, fillvalue, offset):
-    pass
+def pairwise_offset(sequence, fillvalue = "*", offset = 0):
+    fillers = fillvalue * offset
+    
+    if type(sequence) is list:
+        fillers = list(fillers)
+
+    seq1 = fillers + sequence
+    seq2 = sequence + fillers
+
+    result = [(b, a) for (a, b) in zip(seq1, seq2)]
+    return result
