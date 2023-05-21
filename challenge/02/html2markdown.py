840704d499df9c77af7baeb52be107ae5feeb049
Amin Siddiqui <staticVoidMan@users.noreply.github.com>
2023-05-21 18:06:09 +0530

    #02

--- challenge/02/html2markdown.py
+++ challenge/02/html2markdown.py
@@ -1,4 +1,10 @@
+import re
 
 def html2markdown(html):
     '''Take in html text as input and return markdown'''
-    pass
+    html = re.sub(r"</?em>", "*", html)
+    html = re.sub(r"\s+", " ", html)
+    html = re.sub(r"<p>", "\n\n", html).strip()
+    html = re.sub(r"</p>", "", html)
+    html = re.sub(r'<a href="(.+?)">(.+?)</a>', r"[\2](\1)", html)
+    return html
