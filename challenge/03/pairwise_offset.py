c26ee45a2cf922988ecc4cc551f070f510b61d8e
Amin Siddiqui <staticVoidMan@users.noreply.github.com>
2023-05-22 00:10:53 +0530

    #03 - used `itertools`

--- challenge/03/pairwise_offset.py
+++ challenge/03/pairwise_offset.py
@@ -1,11 +1,5 @@
+from itertools import tee, zip_longest, chain
+
 def pairwise_offset(sequence, fillvalue = "*", offset = 0):
-    fillers = fillvalue * offset
-    
-    if type(sequence) is list:
-        fillers = list(fillers)
-
-    seq1 = fillers + sequence
-    seq2 = sequence + fillers
-
-    result = [(b, a) for (a, b) in zip(seq1, seq2)]
-    return result
+    it1, it2 = tee(sequence, 2)
+    return zip_longest(it1, chain(fillvalue * offset, it2), fillvalue=fillvalue)
