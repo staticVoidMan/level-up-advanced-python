b92bda18401707286eecb483b2d82749b8fced19
Amin Siddiqui <staticVoidMan@users.noreply.github.com>
2023-05-23 15:22:12 +0530

    #04 - optimized with nested functions

--- challenge/04/age_slowest_race.py
+++ challenge/04/age_slowest_race.py
@@ -15,28 +15,32 @@ def get_event_time(line):
        parse it and return a tuple of (age at event, race time).
        Assume a year has 365.25 days"""
     
-    found = tuple()
-    if "Jennifer Rhines" in line:
-        racetime_parts = re.findall(r"\d+:\d+", line)[0].split(":")
-        race_time = timedelta(minutes = int(racetime_parts[0]), 
-                              seconds = int(racetime_parts[1]))
-        dates = [datetime.strptime(d, "%d %b %Y") for d in re.findall(r"\d{2} \w+ \d{4}", line)]
-        difference = (dates[0] - dates[1])
-        found = (difference, race_time)
-    return found
+    def get_race_time(line):
+        return re.findall(r"\d+:\d+", line)[0]
+    
+    def get_date(date_):
+        return datetime.strptime(date_, "%d %b %Y")
+    
+    def get_age(dates_):
+        race_date, birth_date = dates_
+        race_date, birth_date = get_date(race_date), get_date(birth_date)
+        return divmod((race_date - birth_date).days, 365.25)
+
+    race_time = get_race_time(line)
+    
+    dates = re.findall(r"\d{2} \w+ \d{4}", line)
+    age = get_age(dates)
+
+    return (age, race_time)
     
 def get_age_slowest_times():
     '''Return a tuple (age, race_time) where:
        age: AyBd is in this format where A and B are integers'''
-    times = []
-    for r in get_data():
-        t = get_event_time(r)
-        if len(t) > 0:
-            times.append(t)
-    slowest = sorted(times, key=lambda x: x[1])[-1]
-    years, days = divmod(slowest[0].days, 365.25)
+    found = [get_event_time(r) for r in get_data() if "Jennifer Rhines" in r]
+    slowest_race_age, slowest_race_time = max(found, key=lambda x: x[1])
+
+    def format_date(age_):
+        return f"{age_[0]:.0f}y{age_[1]:.0f}d"
     
-    age = f"{years:.0f}y{days:.0f}d"
-    time = f"{slowest[1]}"[2:]
-    return (age, time)
+    return (format_date(slowest_race_age), slowest_race_time)
 
