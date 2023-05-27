cb9c10ec032b49e20085538dad815921175a34aa
Amin Siddiqui <staticVoidMan@users.noreply.github.com>
2023-05-27 12:12:12 +0530

    #08

--- challenge/08/linkedin_checker.py
+++ challenge/08/linkedin_checker.py
@@ -12,9 +12,42 @@ def get_linkedin_dict():
     '''Convert specifications into a dict where:
        keys: feature
        values: specs namedtuple'''
-    pass
+    data = {}
+    minimum, maximum = (0, 0)
+    regex = ""
+    
+    for idx, line in enumerate(specifications.splitlines()):
+        try:
+            key, value = line.split(':')
+            key = key.strip()
+            value = value.strip()
+        except:
+            key, value = ("", "")
+        
+        if key == "feature":
+            feature = value
+        elif key == "requirements":
+            minimum, maximum = re.findall(r'\d+', value)
+            minimum = int(minimum)
+            maximum = int(maximum)
+        elif key == "permitted characters":
+            regex = ''.join(value.split())
+            regex = re.compile(rf'^[{regex}]+$')
+        elif key == "login characters":
+            regex = ''.join(value.split()).replace(".", "\\.")
+            regex = re.compile(rf'^[{regex}]+@[{regex}]+\.(com|net|org)$')
+        else:
+            data[feature] = specs(range(minimum, maximum + 1), regex)
+    return data
 
 def check_linkedin_feature(feature_text, url_or_login):
     '''Raise a ValueError if the url_or_login isn't login or custom_url
        If feature_text is valid, return True otherwise return False'''
-    pass
+    rules = get_linkedin_dict()
+    rule = rules.get(url_or_login, None)
+    if rule is None:
+        raise ValueError('Feature needs to be either login or custom_url')
+    else:
+        length = len(feature_text) in rule.range
+        regex = rule.regex.search(feature_text)
+        return length & bool(regex)
--- challenge/08/test_linkedin_checker.py
+++ challenge/08/test_linkedin_checker.py
@@ -10,11 +10,23 @@ def test_correct_custom_url():
     assert check_linkedin_feature('JonathanFernandes', 'custom_url')
     assert check_linkedin_feature('JonathanFernandes2000', 'custom_url')
 
+    # atleast 3 characters
+    assert check_linkedin_feature('jof', 'custom_url')
+    # atmost 100 characters
+    assert check_linkedin_feature('jonathanafernandes20' + '0' * 80, 'custom_url')
+
 def test_incorrect_custom_url():
     assert check_linkedin_feature('jonathanafernande$', 'custom_url') == False
     assert check_linkedin_feature('jon-fernandes2000', 'custom_url') == False
     assert check_linkedin_feature('Jonathan_Fernandes', 'custom_url') == False
     assert check_linkedin_feature('JonathanFernandes2000!!', 'custom_url') == False
+    assert check_linkedin_feature('JonathanFernandes2000!!', 'custom_url') == False
+    
+    # less than 3 characters
+    assert check_linkedin_feature('jf', 'custom_url') == False
+    # more than 100 characters
+    assert check_linkedin_feature('jonathanafernandes20' + '0' * 81, 'custom_url') == False
+    
 
 def test_correct_email():
     assert check_linkedin_feature('jf@gmail.com', 'login')
@@ -23,10 +35,17 @@ def test_correct_email():
     assert check_linkedin_feature('jonathan_fernandes@gmail.com', 'login')
     assert check_linkedin_feature('jonathan.fernandes@gmail.com', 'login')
 
+    # atmost 50 characters
+    assert check_linkedin_feature(f'jonathanfernandes{"0" * 23}@gmail.com', 'login')
+
 def test_incorrect_email():
     assert check_linkedin_feature('jonathangmail.com', 'login') == False
     assert check_linkedin_feature('jonathanfernandes@gmail.biz', 'login') == False
     assert check_linkedin_feature('jonathanfernandes@gmail.co.uk', 'login') == False
+
+    # more than 50 characters
+    assert check_linkedin_feature(f'jonathanfernandes{"0" * 24}@gmail.com', 'login') == False
+    
 
 def test_incorrect_feature():
     with pytest.raises(ValueError):
